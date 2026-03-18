e16fd5e4ffa5b9d14d851c081a3293961bc8c0d9
AI-Kills <alfredo.ingraldo.u@gmail.com>
2026-03-18 20:16:17 +0100

    refactor: remove unnecessary check in server endpoints, add go.mod

--- serv.go
+++ serv.go
@@ -7,19 +7,11 @@ import (
 
 // handler per 2.1 root endpoint
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
 	fmt.Fprintf(w, "status healthy")
 }
 
 // handler per 2.2 saluta il server
 func salutaHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/saluta" {
-		http.NotFound(w, r)
-		return
-	}
 	nome := r.URL.Query().Get("nome")
 	if nome == "" {
 		fmt.Fprintf(w, "ciao")
@@ -29,8 +21,7 @@ func salutaHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/{$}", rootHandler) // suggerimento: invece di "/" (cattura tutto) usi il "/{$}" che cattura solo lo slash
 	http.HandleFunc("/saluta", salutaHandler)
 	http.ListenAndServe(":3000", nil)
-
 }
